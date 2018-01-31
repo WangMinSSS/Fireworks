dcee654cfb99d03630582888631be81e403b487f
WangMinSSS <wangmin.min@qq.com>
2018-01-31 21:48:48 +0800

    1

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('demo.html')
+    # return render_template('demo.html')
+    return render_template('Fireworks.html')
 
 if __name__ == '__main__':
-    # app.run()
-    app.run(host='0.0.0.0', port=9999)
+    app.run()
+    # app.run(host='0.0.0.0', port=9999)
