bee2c4e357ec1833e905e271a189fc5a6d30ec28
wangmin <969821285@qq.com>
2018-02-15 18:02:46 +0800

    1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # return render_template('demo.html')
-    return render_template('Fireworks.html')
+    return render_template('demo.html')
 
 if __name__ == '__main__':
     #app.run()
